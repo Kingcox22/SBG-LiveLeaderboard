ec9ab3e5f7057133a49b5c122a5c5cb164758601
Kingcox22 <Kingcox22@gmail.com>
2026-03-20 22:50:36 -0400

    Delete obj directory

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Leaderboard
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "SBG-LiveLeaderboard";
-        public const string PLUGIN_NAME = "Super Battle Golf Live Leaderboard";
-        public const string PLUGIN_VERSION = "1.1.1";
-    }
-}
